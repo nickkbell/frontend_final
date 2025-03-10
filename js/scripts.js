b8ff503ddb8c0dd5e6b005df5eb563a1bf5ec88f
nick <n.belitskiy@infomir.com>
2025-03-10 15:27:17 +0200

    add all files

--- js/scripts.js
+++ js/scripts.js
@@ -58,65 +58,43 @@ $(document).ready(function(){
 
   
   (function($) { "use strict";
-    // Инициализация после загрузки документа
     $(document).ready(function() {
-        
-        // Функция для добавления анимации при открытии/закрытии меню Bootstrap
         $('.navbar-toggler').on('click', function() {
-            // Добавляем/удаляем класс для анимации или дополнительных стилей
             $('body').toggleClass('nav-active');
-            
-            // Если нужны дополнительные эффекты при открытии/закрытии меню
+
             if ($('body').hasClass('nav-active')) {
-                // Действия при открытии меню
-                console.log('Меню открыто');
-                
-                // Пример: затемнение фона при открытом меню
                 $('.hero-section').addClass('dimmed');
             } else {
-                // Действия при закрытии меню
-                console.log('Меню закрыто');
-                
-                // Удаляем затемнение
                 $('.hero-section').removeClass('dimmed');
             }
         });
         
-        // Закрытие меню при клике по пункту меню (опционально)
         $('.navbar-collapse .nav-link').on('click', function() {
-            if ($(window).width() < 992) { // Только на мобильных
-                $('.navbar-toggler').click(); // Эмулируем клик по кнопке закрытия
+            if ($(window).width() < 992) { 
+                $('.navbar-toggler').click();
             }
         });
         
-        // Переключение светлой/темной темы (оставляем без изменений)
         $("#switch").on('click', function () {
             if ($("body").hasClass("light")) {
                 $("body").removeClass("light");
                 $("#switch").removeClass("switched");
                 
-                // Дополнительно: меняем цвета в навигации при смене темы
                 updateNavColors(false);
             }
             else {
                 $("body").addClass("light");
                 $("#switch").addClass("switched");
-                
-                // Дополнительно: меняем цвета в навигации при смене темы
+
                 updateNavColors(true);
             }
         });
-        
-        // Вспомогательная функция для обновления цветов навигации при смене темы
+
         function updateNavColors(isLight) {
             if (isLight) {
-                // Светлая тема
                 $('.navbar').addClass('navbar-light').removeClass('navbar-dark');
-                // Другие изменения для светлой темы...
             } else {
-                // Темная тема
                 $('.navbar').addClass('navbar-dark').removeClass('navbar-light');
-                // Другие изменения для темной темы...
             }
         }
     });
